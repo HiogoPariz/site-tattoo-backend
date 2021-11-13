6b776e490a961f3ea1368a699cbf0a5e1d6fcc06
Hiogo de Pariz Cavalheiro <hiogopariz@gmail.com>
2021-11-13 20:41:36 -0300

    Staging (#7)
    * fix: import
    
    * feat: find on tatuador controller (#4)
    
    * feat: find on tatuador controller
    
    * fix: jest imports
    
    * ci: actions
    
    * Feature/commitlint (#5)
    
    * ci: husky
    
    * ci: commitlint
    
    * refactor: lint config.js
    
    * ci: lint
    
    * ci: commitlint after install
    
    * ci: lint
    
    * ci: lint
    
    * ci(release): added release (#6)

---
+++ .releaserc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  branches: [
+    'master',
+    {
+      name: 'staging',
+      prerelease: true,
+    },
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/git',
+    '@semantic-release/github',
+  ],
+};
